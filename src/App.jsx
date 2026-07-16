b2c53abfc1a04af92dd69e70c9b084fd75e067d8
Abinashrout244 <routabinash3775@gmail.com>
2026-07-16 10:08:28 +0530

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <p className="text-3xl font-semibold bg-red-200 text-blue-500 ">Hi jilu</p>
+  );
+}
+
+export default App;
